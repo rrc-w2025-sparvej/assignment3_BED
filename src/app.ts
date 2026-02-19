78cd621fb9ae937c9c1064a6ae3718b92a25bc5f
sania parvej <sparvej@rrc.ca>
2026-02-19 00:25:44 -0600

    feat: register event routes in app.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
-
+import eventRoutes from "./api/v1/routes/eventRoutes";
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// Register API routes
+app.use("/api/v1/events", eventRoutes);
 
 
 export default app;
