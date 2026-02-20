7cf02d4bb76ca4d3f2f5031ccb795b5c22fd80d9
sania parvej <sparvej@rrc.ca>
2026-02-19 20:28:38 -0600

    feat: implement update and delete logiv.

--- src/api/v1/services/eventService.ts
+++ src/api/v1/services/eventService.ts
@@ -18,3 +18,19 @@ export const getAllEvents = (): Event[] => events;
 
 export const getEventById = (id: string): Event | undefined =>
   events.find(e => e.id === id);
+
+export const updateEvent = (id: string, update: Partial<Event>): Event | null => {
+  const event = events.find(e => e.id === id);
+  if (!event) return null;
+
+  Object.assign(event, update);
+  return event;
+};
+
+export const deleteEvent = (id: string): boolean => {
+  const index = events.findIndex(e => e.id === id);
+  if (index === -1) return false;
+
+  events.splice(index, 1);
+  return true;
+};
