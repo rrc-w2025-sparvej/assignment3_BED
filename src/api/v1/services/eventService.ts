cac117e754a5b0e02a225e8055ecc5d7fade9217
sania parvej <sparvej@rrc.ca>
2026-02-19 20:09:07 -0600

    feat: add event service layer.

---
+++ src/api/v1/services/eventService.ts
@@ -0,0 +1,9 @@
+import { Event } from "../models/Event";
+
+export const createEvent = (event: Event): Event => {
+  return event;
+};
+
+export const getAllEvents = (): Event[] => {
+  return [];
+};
