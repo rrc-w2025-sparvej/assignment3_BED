51a08042001eaac615bbb26f18e2459cf628bec2
sania parvej <sparvej@rrc.ca>
2026-02-19 20:57:36 -0600

    feat: add GET endpoints.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import { validateRequest } from "../middleware/validate";
 import { eventSchemas } from "../validation/eventSchemas";
-import { createEventHandler } from "../controllers/eventController";
+import {
+  createEventHandler,
+  getAllEventsHandler,
+  getEventByIdHandler,
+ 
+} from "../controllers/eventController";
 
 const router = express.Router();
 
 
 router.post("/", validateRequest(eventSchemas.create), createEventHandler);
+router.get("/", getAllEventsHandler);
+router.get("/:id", getEventByIdHandler);
+
+
 
 export default router;
